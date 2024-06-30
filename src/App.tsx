bd96d8e6dd73674a3449557c52b853703417e37b
resty mendoza <kylemendoza67@gmail.com>
2024-06-30 15:13:30 +0800

    done todolist

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 type Todo = {
   note: string;
+  editable: boolean;
 };
 
 function App() {
@@ -11,28 +12,71 @@ function App() {
   const [todo, setTodo] = useState<string>("");
 
   const addTodo = (note: string) => {
-    const newTodo: Todo = { note };
-    setTodoList((prevTodoList) => {
-      if (prevTodoList) {
-        return [...prevTodoList, newTodo];
-      } else {
-        return [newTodo];
-      }
-    });
+    const newTodo: Todo = { note, editable: false };
+    if (note) {
+      setTodoList((prevTodoList) => {
+        if (prevTodoList) {
+          return [...prevTodoList, newTodo];
+        } else {
+          return [newTodo];
+        }
+      });
+      setTodo("");
+    }
   };
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodo(e.target.value);
   };
 
+  const onDelete = (index: number) => {
+    setTodoList((prevTodoList) =>
+      (prevTodoList ?? []).filter((_, i) => i !== index)
+    );
+  };
+
+  const onUpdate = (index: number) => {
+    setTodoList((prevTodoList) =>
+      (prevTodoList ?? []).map((item, key) => {
+        if (key === index) {
+          return { ...item, editable: true };
+        }
+        return item;
+      })
+    );
+  };
+
+  const onEdit = (index: number, e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoList((prevTodoList) =>
+      (prevTodoList ?? []).map((item, key) => {
+        if (key === index) {
+          return { ...item, note: e.target.value };
+        }
+        return item;
+      })
+    );
+  };
+
+  const onSave = (index: number) => {
+    setTodoList((prevTodoList) =>
+      (prevTodoList ?? []).map((item, key) => {
+        if (key === index) {
+          return { ...item, editable: false };
+        }
+        return item;
+      })
+    );
+  };
+
   return (
     <main className="bg-[#8ecae6] h-screen flex justify-center items-center">
       <div className="bg-[#023047] w-[30%] h-[80%] flex flex-col gap-4 items-center p-5 rounded-sm">
-        <p className="text-white text-2xl">Get Things Done!</p>
-        <div className="add-todo-container flex w-full">
+        <p className="text-2xl text-white">Get Things Done!</p>
+        <div className="flex w-full add-todo-container">
           <input
             className="w-full border-[1px] border-[#219ebc] bg-inherit p-1 text-sm text-white"
             placeholder="What is the task today?"
+            value={todo}
             onChange={onChange}
           />
           <button
@@ -42,22 +86,49 @@ function App() {
             Add Task
           </button>
         </div>
-        <div className="todo-container w-full flex flex-col gap-4 items-center h-full overflow-y-auto no-scrollbar">
+        <div className="flex flex-col items-center w-full h-full gap-4 overflow-y-auto todo-container no-scrollbar">
           {todoList ? (
             todoList.map((todo: Todo, index) => (
               <div
-                className="todo w-full bg-[#8ecae6] p-4 rounded-sm flex justify-between items-center"
+                className="todo w-full bg-[#8ecae6] p-3 rounded-sm flex justify-between items-center"
                 key={index}
               >
-                <p className="text-inherit">{todo.note}</p>
-                <div className="tools flex justify-center items-center gap-2">
-                  <FontAwesomeIcon icon={faPenToSquare} />
-                  <FontAwesomeIcon icon={faTrashCan} />
-                </div>
+                {todo.editable ? (
+                  <input
+                    className="w-full border-[1px] border-[#219ebc] bg-inherit p-1 text-sm text-white"
+                    value={todo.note}
+                    onChange={(e) => onEdit(index, e)}
+                  />
+                ) : (
+                  <p className="text-inherit">{todo.note}</p>
+                )}
+
+                {todo.editable ? (
+                  <button
+                    className="bg-[#023047] px-2 py-1 rounded-sm text-white font-semibold text-sm"
+                    onClick={() => onSave(index)}
+                  >
+                    Save
+                  </button>
+                ) : (
+                  <div className="flex items-center justify-center gap-2 tools">
+                    <FontAwesomeIcon
+                      icon={faPenToSquare}
+                      onClick={() => onUpdate(index)}
+                      className="cursor-pointer"
+                    />
+                    <FontAwesomeIcon
+                      icon={faTrashCan}
+                      color="red"
+                      onClick={() => onDelete(index)}
+                      className="cursor-pointer"
+                    />
+                  </div>
+                )}
               </div>
             ))
           ) : (
-            <p className="text-white text-xs italic">No Todo list for today.</p>
+            <p className="text-xs italic text-white">No Todo list for today.</p>
           )}
         </div>
       </div>
